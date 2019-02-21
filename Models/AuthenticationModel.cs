8fce333d3f67468d468df59ea0e5ef5d66f9b2b0
Mateusz Czernek <czernek.mateusz@gmail.com>
2019-02-21 11:43:43 +0100

    Initial commit

---
+++ Models/AuthenticationModel.cs
@@ -0,0 +1,9 @@
+namespace AWSS3
+{        
+    public class AuthenticationModel
+    {
+        public string AccessKey { get; set; }
+
+        public string SecretKey { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace AWSS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S3AWSConfiguration s3AWSConfiguration = new S3AWSConfiguration();
+
+            S3AWSService s3AWSApi = new S3AWSService(s3AWSConfiguration.AuthenticationConfiguration.AccessKey, s3AWSConfiguration.AuthenticationConfiguration.SecretKey);
+
+        }
+    }
+}
